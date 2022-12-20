45b81e65797ed48498d7c89f701f2154b0e89952
Jakub Dadzibóg <jakubdadz@wp.pl>
2022-12-20 18:43:27 +0100

    fix token expiration while running bot

--- bot.py
+++ bot.py
@@ -10,14 +10,16 @@ def main():
     # Create API object
     API = Api(CLIENT_ID, CLIENT_SECRET)
 
-    if API.check_token() == False:
-        result = API.get_code()
-        API.await_for_access_token(int(result['interval']), result['device_code'])
-
-    # Run scheduler for messages
     update_period = 60
-    scheduler = Scheduler(update_period, API.access_token)
-    scheduler.main()        
+    
+    while True:
+        if API.check_token() == False:
+            result = API.get_code()
+            API.await_for_access_token(int(result['interval']), result['device_code'])
+
+        # Run scheduler for messages
+        scheduler = Scheduler(update_period, API.access_token)
+        scheduler.main()        
         
 if __name__ == '__main__':
     main()
